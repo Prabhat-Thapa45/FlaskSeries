f286e87b646b546290ccdea6ad0d601505521618
Prabhat Thapa <akoup66@gmail.com>
2021-09-24 15:10:02 +0545

    Update check.py

--- src/check.py
+++ src/check.py
@@ -33,7 +33,7 @@ def int_convertor(value: str) -> int:
         return 0
 
 
-def float_convertor(value: str) -> float:
+def float_convertor(value: str) -> float or str:
     """Converts str type to float type
     Args:
         value (str): strings to be converted to int type
@@ -53,7 +53,7 @@ def float_convertor(value: str) -> float:
 def validate_purchase_input(quantity, bouquet_size, available_in_stock, username, flower_name, price):
     if quantity == 0:
         return ["No items were added into your cart", "danger",  bouquet_size]
-    if available_in_stock < quantity:
+    elif available_in_stock < quantity:
         return [f"We can't meet your demand, we only have {available_in_stock} of them in stock", "danger",
                 bouquet_size]
     elif bouquet_size >= quantity > 0:
