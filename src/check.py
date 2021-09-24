fd174348f8a21f8d9ac2d40266a1b081ee54eaa4
Prabhat Thapa <akoup66@gmail.com>
2021-09-24 13:19:04 +0545

    Update check.py

--- src/check.py
+++ src/check.py
@@ -1,3 +1,6 @@
+from src.execute_sql import query_handler_no_fetch
+
+
 def check_flower_by_name(results: tuple, flower_name: str) -> bool:
     """Checks if the given flower_name is available in results if yes returns True else False
     Args:
@@ -30,3 +33,19 @@ def int_convertor(value: str) -> int:
         return 0
 
 
+def validate_purchase_input(quantity, bouquet_size, available_in_stock, username, flower_name, price):
+    if quantity == 0:
+        return ["No items were added into your cart", "danger",  bouquet_size]
+    if available_in_stock < quantity:
+        return [f"We can't meet your demand, we only have {available_in_stock} of them in stock", "danger", 
+                bouquet_size]
+    elif bouquet_size >= quantity > 0:
+        bouquet_size -= quantity
+        query = "INSERT INTO orders(username, flower_name, quantity, price) VALUES(%s, %s, %s, %s)"
+        values = (username, flower_name, quantity, price)
+        query_handler_no_fetch(query, values)
+        return ["Item added to cart", "success", bouquet_size]
+    elif bouquet_size < quantity:
+        return ["You have exceeded bouquet size", "danger", bouquet_size]
+    elif quantity < 0:
+        return ["Order quantity cannot be less than zero", "danger", bouquet_size]
